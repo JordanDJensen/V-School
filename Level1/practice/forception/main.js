c2e3df4a4223da305afe90644970405af56194da
Jordan Jensen <jojo.jensen03@gmail.com>
2025-06-10 20:56:39 -0600

    Use array methods to perform this output.

---
+++ Level1/practice/forception/main.js
@@ -0,0 +1,23 @@
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+function forception(people, alphabet){
+    let result = [];
+
+    for(let i = 0; i < people.length; i++){
+        result.push(people[i] + ":");
+        for(let j = 0; j < alphabet.length; j++){
+            result.push(alphabet[j].toUpperCase());
+        }
+    }
+
+    return result;
+}
+
+console.log(forception(people, alphabet));
+// Output:
+//["Jon:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 
+// "Jacob:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 
+// "Jingle:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 
+// "Heimer:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 
+// "Schmidt:", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
