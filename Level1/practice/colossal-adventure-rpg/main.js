c3d468d4330eec0d14826e1a0dd52f8a0d41a926
Jordan Jensen <jojo.jensen03@gmail.com>
2025-08-25 21:28:01 -0600

    Started working on creating a rpg game using javascript.

---
+++ Level1/practice/colossal-adventure-rpg/main.js
@@ -0,0 +1,14 @@
+const readlineSync = require('readline-sync');
+
+const name = readlineSync.question("What is your name player? ")
+
+console.log("Welcome to the Rebellion " + name + "! We will be flying our starship to the Tatooine system to locate two droids" + 
+    " which contain the plans of a secret weapon the Empire is building. In your pursuit, you may encounter Jawas, Tusken Raiders " + 
+   "and Stormtroopers. Be safe out there and remember... May the Force be with you.")
+
+console.log("")
+
+console.log("As you were approaching Tatoonie your starship was shot down by the Imperial Star Destroyer but you were able to escape " + 
+    "in an escape pod and landed on Tatoonie. Now, it is time to find the missing droids."
+)
+
