3e97cc5843ad66068dbe131848dadbfc0c479148
Jordan Jensen <jojo.jensen03@gmail.com>
2025-10-07 21:20:12 -0600

    added multiplication to project."

--- level2/practice/html-calculator/main.js
+++ level2/practice/html-calculator/main.js
@@ -1,10 +1,10 @@
 
 
-document.add.addEventListener("submit", function(e){
+document.forms.add.addEventListener("submit", function(e){
     e.preventDefault();
-    let num1 = parseInt(document.add.add1.value)
+    let num1 = parseInt(document.forms.add.add1.value)
     
-    let num2 = parseInt(document.add.add2.value)
+    let num2 = parseInt(document.forms.add.add2.value)
     
     let sum = num1 + num2
     
@@ -19,10 +19,10 @@ document.add.addEventListener("submit", function(e){
 
 })
 
-document.sub.addEventListener("submit", function(e){
+document.forms.sub.addEventListener("submit", function(e){
     e.preventDefault();
-    let num1 = parseInt(document.sub.sub1.value)
-    let num2 = parseInt(document.sub.sub2.value)
+    let num1 = parseInt(document.forms.sub.sub1.value)
+    let num2 = parseInt(document.forms.sub.sub2.value)
 
     let subtract = num1 - num2
 
@@ -34,4 +34,21 @@ document.sub.addEventListener("submit", function(e){
     }
     total.textContent = "total: " + subtract
 
+})
+
+document.forms.mul.addEventListener("submit", function(e) {
+    e.preventDefault();
+    let num1 = parseInt(document.forms.mul.mul1.value)
+    let num2 = parseInt(document.forms.mul.mul2.value)
+
+    let multiple = num1 * num2
+
+    const container = document.getElementById("mul-container")
+    let total = container.querySelector("h1")
+
+    if(!total){
+        total = document.createElement("h1")
+        container.appendChild(total)
+    }
+    total.textContent = "Total: " + multiple
 })
