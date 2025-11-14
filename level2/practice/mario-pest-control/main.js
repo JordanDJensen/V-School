5a656201c1bbc4fce0ce5988537eb6380c6e5d09
Jordan Jensen <jojo.jensen03@gmail.com>
2025-11-13 18:07:21 -0700

    Add code where total of different baddies is added to final cost. Added footer and used css to make it stick

--- level2/practice/mario-pest-control/main.js
+++ level2/practice/mario-pest-control/main.js
@@ -5,14 +5,23 @@ submit.addEventListener("click", () => {
     let countBobombs = document.getElementsByName("bobombs")[0].value;
     let countCheepCheep = document.getElementsByName("cheepCheep")[0].value;
 
-   let costGoombas = countGoombas * 5
+   let costGoombas = countGoombas * 5;
+   let costBobombs = countBobombs* 7;
+   let costCheepCheep = countCheepCheep * 11;
+   let sum = costGoombas + costBobombs + costCheepCheep;
+
+    // Check if total div already exists and remove it
+    let existingTotal = document.querySelector(".total");
+    if (existingTotal) {
+        existingTotal.remove();
+    }
 
     let total = document.createElement("div");
     total.className = "total";
     
   
     let totalText = document.createElement("h2");
-    totalText.textContent = `Total: ${costGoombas}`;
+    totalText.textContent = `Total: ${sum}`;
     
     total.appendChild(totalText);
     
