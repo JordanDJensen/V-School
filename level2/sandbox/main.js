018919d744c269908a06219cc4f980c3a3f5bf19
Jordan Jensen <jojo.jensen03@gmail.com>
2025-12-28 10:25:59 -0700

    practice with localStorage

--- level2/sandbox/main.js
+++ level2/sandbox/main.js
@@ -1,6 +1,34 @@
-document.animals.addEventListener("submit", function(e){
-    e.preventDefault()
+// document.animals.addEventListener("submit", function(e){
+//     e.preventDefault()
 
-    var animal = document.animals.favorite.value
-    console.log(animal)
-})
+//     var animal = document.animals.favorite.value
+//     console.log(animal)
+// })
+
+
+///////// Pracitce with localStorage /////////////// 
+
+var obj = {
+    name: "Billy",
+    age: 45,
+    cars: [
+        {
+            year: 2002,
+            make: "Toyota",
+            model: "Camry"
+        },
+        {
+            year: 2018,
+            make: "Tesla",
+            model: "Roadster"
+        }
+    ]
+}
+
+localStorage.user = JSON.stringify(obj)
+
+let user = JSON.parse(localStorage.user)
+
+console.log(user)
+
+///////// Pracitce with localStorage /////////////
