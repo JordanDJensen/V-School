d45249ce57d0365f982c16a6011e7f49eee81a36
Jordan Jensen <jojo.jensen03@gmail.com>
2026-01-19 21:31:47 -0700

    play more with arrays

--- level2/practice/array-practice/main.js
+++ level2/practice/array-practice/main.js
@@ -19,8 +19,41 @@ let names =  ["john", "JACOB", "jinGleHeimer", "schmidt"]
 let capNameResults = names.map(function(name){
     let results = name.toLocaleLowerCase();
     
-    return results.charAt(0).toUpperCase() 
+    return results.charAt(0).toUpperCase().charAt(0).toUpperCase() + results.substring(1);
 })
 
 console.log(capNameResults)
+
+// Make an array of strings of the names
+let namesWithAge = [
+    {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+]
+
+
+
+let result = namesWithAge.map(function(user){
+    return user.name
+})
+
+console.log(result)
+
 
