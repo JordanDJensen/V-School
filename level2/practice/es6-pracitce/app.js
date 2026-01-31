1a75d17c24d75bdeefc7acd8a6613556ee4a18da
Jordan Jensen <jojo.jensen03@gmail.com>
2026-01-30 18:38:49 -0700

    practice with es6; arrow function

---
+++ level2/practice/es6-pracitce/app.js
@@ -0,0 +1,94 @@
+const name = "John"
+let age = 101
+
+function runForLoop(pets) {
+    let petObjects = []
+    for (let i = 0; i < pets.length; i++) {
+        let pet = { type: pets[i] }
+        let name;
+        if (pets[i] === "cat") {
+            name = "fluffy"
+        } else {
+            name = "spot"
+        }
+        console.log("pet name: ", name)
+        pet.name = name
+        petObjects.push(pet)
+    }
+    console.log("man name: ", name)
+    return petObjects
+}
+
+runForLoop(["cat", "dog"])
+
+
+// Arrow Functions
+const carrots = ["bright orange", "ripe", "rotten"]
+
+let mapVegetables = arr =>arr.map(carrot =>({ type: "carrot", name: carrot }));
+
+console.log(mapVegetables(carrots))
+
+
+const people = [
+    {
+        name: "Princess Peach",
+        friendly: false
+    },
+    {
+        name: "Luigi",
+        friendly: true
+    },
+    {
+        name: "Mario",
+        friendly: true
+    },
+    {
+        name: "Bowser",
+        friendly: false
+    }
+]
+
+let filterForFriendly = arr =>  arr.filter(person => person.friendly)
+
+console.log(filterForFriendly(people))
+
+
+let doMathSum = (a, b) => {return a + b}
+
+let produceProduct = (a, b) => {return a * b}
+
+console.log(doMathSum(2,2))
+console.log(produceProduct(3,3))
+
+let greeting = (firstName = "Jane", lastName = "Doe", age = 100) =>{
+    return `Hi ${firstName} ${lastName}, how does it feel to be ${age}?`
+}
+
+console.log(greeting("Jordan", "Jensen", 31))
+
+
+const animals = [
+   {
+       type: "dog",
+       name: "theodore"
+   },
+   {
+       type: "cat",
+       name: "whiskers"
+   },
+   {
+       type: "pig",
+       name: "piglette"
+   },
+   {
+       type: "dog",
+       name: "sparky"
+   }
+];
+
+const filterForDogs= arr => arr.filter(animal =>  animal.type == "dog");
+
+
+
+console.log(filterForDogs(animals))
