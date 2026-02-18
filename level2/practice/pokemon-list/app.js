f7beb587343ef46571724e518823fd76f143d278
Jordan Jensen <jojo.jensen03@gmail.com>
2026-02-17 17:11:18 -0700

    play with axios and use get call to gather all pokemon and display

---
+++ level2/practice/pokemon-list/app.js
@@ -0,0 +1,15 @@
+
+
+axios.get("https://api.vschool.io/pokemon").then((result) => {
+    const pokemonList = result.data.objects[0].pokemon
+    for(let i = 0; i < pokemonList.length; i++){
+        const h1 = document.createElement("h1")
+        h1.textContent = pokemonList[i].name
+
+        document.body.appendChild(h1)
+    }
+}).catch((err) => {
+    console.log(err)
+});
+
+
