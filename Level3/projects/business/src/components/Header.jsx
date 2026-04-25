16ea8d03cb476a0b801b0c8fe93f0e1cdb709a1c
Jordan Jensen <jojo.jensen03@gmail.com>
2026-04-24 18:11:23 -0600

    Project on business

---
+++ Level3/projects/business/src/components/Header.jsx
@@ -0,0 +1,17 @@
+function Header(){
+    return (
+       <>
+       <div className="nav-bar">
+        <h2>Mister Drone Guy</h2>
+            <nav className="nav-links">
+                <a>Services</a>
+                <a>Portfolio</a>
+                <a>About</a>
+                <a>Contact</a>
+            </nav>
+       </div>
+       </>
+    )
+}
+
+export default Header
---
+++ Level3/projects/business/src/components/Main.jsx
@@ -0,0 +1,19 @@
+function Main() {
+    return(
+        <div className="gridForm">
+            <div className="div1">
+                <h1>From Listings to Brands—We Elevate It All.</h1>
+            </div>
+            <div className="div2">
+                <p>We specialize in premium aerial photography and cinematic videography 
+                    that highlights the beauty, scale, and uniqueness of every property. 
+                    Our visuals are crafted to elevate listings and create a lasting impression.</p>
+
+                <button>Request A Quote</button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Main
